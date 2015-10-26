fb66788e3b3d35a10614e0efc52d1ac6debc5424
anagramtogo <samuelschoe+bitbucket@gmail.com>
2015-10-25 20:34:38 -0700

    Removed modified date.

--- LaunchIssac.ps1
+++ LaunchIssac.ps1
@@ -1,6 +1,5 @@
 #launch the BOI:R with a modified hud. The program checks the status of BOI:R, and closes the hud if BOI:R is not running
 #Written by Sam Schoeller
-#Last Modified 10/20/15
 #References:
 #http://powershell.com/cs/blogs/tips/archive/2011/08/25/check-whether-a-program-is-running.aspx
 #http://blogs.technet.com/b/heyscriptingguy/archive/2014/04/29/powershell-looping-using-the-foreach-object-cmdlet.aspx
