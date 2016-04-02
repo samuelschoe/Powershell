cdf0bbc590f238529d64ebd0953f4d276ee75b5d
anagramtogo <samuelschoe+bitbucket@gmail.com>
2016-04-02 16:43:34 -0700

    A way to copy stardew valley saves from laptop to PC

---
+++ Copy stardew save.ps1
@@ -0,0 +1,8 @@
+﻿$userprofile=$Env:USERPROFILE
+ 
+
+remove-item "$userprofile\ownCloud\Freenas\Games\stardiew save\blips_118379463" -Recurse
+start-sleep -s 5
+Copy-Item "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463" -Recurse "$userprofile\ownCloud\Freenas\Games\stardiew save"
+start-sleep -s 5
+rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463" blipssavebackup_$date.bak
