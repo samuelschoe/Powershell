18315da8cc9642431ae3fb244bbbdf541dc39879
anagramtogo <samuelschoe+bitbucket@gmail.com>
2016-04-02 17:02:35 -0700

    copy to the new pc

---
+++ Copy stardew save to new.ps1
@@ -0,0 +1,6 @@
+﻿$userprofile=$Env:USERPROFILE
+
+ 
+remove-item $userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463" -Recurse
+start-sleep -s 3"
+Copy-Item "$userprofile\ownCloud\Freenas\Games\stardiew save\blips_118379463" -Recurse "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463"
