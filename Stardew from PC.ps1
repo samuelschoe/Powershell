23051e6779a41dc9794aac4e45c71374a32cae57
anagramtogo <samuelschoe+bitbucket@gmail.com>
2016-04-04 18:13:27 -0700

    rename and add

---
+++ Stardew from PC.ps1
@@ -0,0 +1,9 @@
+﻿$userprofile=$Env:USERPROFILE
+$date=Get-Date -format "yyyyMMddmmss"
+ 
+
+remove-item "$userprofile\ownCloud\Freenas\Games\stardiew save\blips_118379463" -Recurse
+start-sleep -s 3
+Copy-Item "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463" -Recurse "$userprofile\ownCloud\Freenas\Games\stardiew save"
+start-sleep -s 3
+rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\blips_118379463" -Force "savebackup_$date.bak"
