b73599de4ebc466134c97882af7d89933de4df94
no thanks <no thanks>
2016-04-05 13:39:56 -0700

    added variable names

--- Stardew from PC.ps1
+++ Stardew from PC.ps1
@@ -1,9 +1,10 @@
-﻿$userprofile=$Env:USERPROFILE
+$userprofile=$Env:USERPROFILE
 $date=Get-Date -format "yyyyMMddmmss"
- 
+$savename="blips_118379463"
 
-remove-item "$userprofile\ownCloud\Freenas\Games\stardiew save\blips_118379463" -Recurse
+remove-item "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" -Recurse
 start-sleep -s 3
-Copy-Item "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463" -Recurse "$userprofile\ownCloud\Freenas\Games\stardiew save"
+Copy-Item   "$userprofile\AppData\Roaming\StardewValley\Saves\$savename" -Recurse "$userprofile\ownCloud\Freenas\Games\stardiew save"
 start-sleep -s 3
-rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\blips_118379463" -Force "savebackup_$date.bak"
+rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\$savename" -Force "blips_118379463_$date.bak"
+move-item   "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\*.*" "$userprofile\AppData\Roaming\StardewValley\Backupsaves" -Force
