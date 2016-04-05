53f86989a573502d7679c5c32a45b153a264fcfd
anagramtogo <samuelschoe+bitbucket@gmail.com>
2016-04-04 18:22:36 -0700

    clean up redundant files

--- Copy stardew save.ps1
+++
@@ -1,9 +0,0 @@
-﻿$userprofile=$Env:USERPROFILE
-$date=Get-Date -format "yyyyMMdd"
- 
-
-remove-item "$userprofile\ownCloud\Freenas\Games\stardiew save\blips_118379463" -Recurse
-start-sleep -s 5
-Copy-Item "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463" -Recurse "$userprofile\ownCloud\Freenas\Games\stardiew save"
-start-sleep -s 5
-rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\blips_118379463" "savebackup_$date.bak"
--- Stardew to PC.ps1
+++ Stardew to PC.ps1
@@ -1,6 +1,8 @@
-﻿$userprofile=$Env:USERPROFILE
+$userprofile=$Env:USERPROFILE
 
- 
-remove-item $userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463" -Recurse
-start-sleep -s 3"
-Copy-Item "$userprofile\ownCloud\Freenas\Games\stardiew save\blips_118379463" -Recurse "$userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463"
+
+remove-item $userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\blips_118379463
+remove-item $userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\savebackup* -Force
+remove-item $userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\SaveGameInfo
+start-sleep -s 3
+Copy-Item "$userprofile\ownCloud\Freenas\Games\stardiew save\blips_118379463" -Recurse "$userprofile\AppData\Roaming\StardewValley\Saves\"
