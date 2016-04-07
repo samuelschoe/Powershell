b275d32a38182ca55f75c75fbac4e086ce2e2a89
no thanks <no thanks>
2016-04-06 17:31:48 -0700

    latest version with more variables

--- Stardew from PC.ps1
+++ Stardew from PC.ps1
@@ -1,10 +1,12 @@
 $userprofile=$Env:USERPROFILE
 $date=Get-Date -format "yyyyMMddmmss"
 $savename="blips_118379463"
+$localbackupsave="$savename_$date.bak"
 
 remove-item "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" -Recurse
 start-sleep -s 3
 Copy-Item   "$userprofile\AppData\Roaming\StardewValley\Saves\$savename" -Recurse "$userprofile\ownCloud\Freenas\Games\stardiew save"
 start-sleep -s 3
-rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\$savename" -Force "blips_118379463_$date.bak"
-move-item   "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\*.*" "$userprofile\AppData\Roaming\StardewValley\Backupsaves" -Force
+rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\$savename" -Force "$savename_$date.bak"
+remove-item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\SaveGame*"
+move-item   "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\$localbackupsave" "$userprofile\AppData\Roaming\StardewValley\Backupsaves" -Force
--- Stardew to PC.ps1
+++ Stardew to PC.ps1
@@ -1,8 +1,9 @@
 $userprofile=$Env:USERPROFILE
+$savename="blips_118379463"
 
 
-remove-item $userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\blips_118379463
-remove-item $userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\savebackup* -Force
-remove-item $userprofile\AppData\Roaming\StardewValley\Saves\blips_118379463\SaveGameInfo
+remove-item $userprofile\AppData\Roaming\StardewValley\Saves\$savename\$savename
+remove-item $userprofile\AppData\Roaming\StardewValley\Saves\$savename\savebackup* -Force
+remove-item $userprofile\AppData\Roaming\StardewValley\Saves\$savename\SaveGameInfo
 start-sleep -s 3
-Copy-Item "$userprofile\ownCloud\Freenas\Games\stardiew save\blips_118379463" -Recurse "$userprofile\AppData\Roaming\StardewValley\Saves\"
+Copy-Item "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" -Recurse "$userprofile\AppData\Roaming\StardewValley\Saves\" -Force
