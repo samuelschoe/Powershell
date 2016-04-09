4f6e0b4348979e6cd065ee43b9eb0874484a095e
anagramtogo <samuelschoe+bitbucket@gmail.com>
2016-04-08 22:22:32 -0700

    cleaned up save location and moved delete to toStardew

--- FromStardew.ps1
+++ FromStardew.ps1
@@ -2,11 +2,18 @@ $userprofile=$Env:USERPROFILE
 $date=Get-Date -format "yyyyMMddmmss"
 $savename="blips_118379463"
 $localbackupsave="$savename_$date.bak"
+$savegameinfosave="SaveGameInfo_$date.bak"
 
-remove-item "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" -Recurse
-start-sleep -s 3
+
 Copy-Item   "$userprofile\AppData\Roaming\StardewValley\Saves\$savename" -Recurse "$userprofile\ownCloud\Freenas\Games\stardiew save"
 start-sleep -s 3
 rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\$savename" -Force "$savename_$date.bak"
-remove-item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\SaveGame*"
+rename-Item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\SaveGameInfo" -Force "SaveGameInfo_$date.bak"
 move-item   "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\$localbackupsave" "$userprofile\AppData\Roaming\StardewValley\Backupsaves" -Force
+move-item   "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\$savegameinfosave" "$userprofile\AppData\Roaming\StardewValley\Backupsaves" -Force
+
+
+#covered in the Startdew to.ps1 script
+#remove-item "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" -Recurse
+#start-sleep -s 3
+#remove-item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename\SaveGame*"
