bfa464c9f10ee174d65c3673142b2dabb0394b8d
anagramtogo <samuelschoe+bitbucket@gmail.com>
2016-04-10 11:51:33 -0700

    reverted back to a working date format

--- FromStardew.ps1
+++ FromStardew.ps1
@@ -1,5 +1,5 @@
 $userprofile=$Env:USERPROFILE
-$date=Get-Date -format "MM-dd-yyyy::h:mm-ss"
+$date=Get-Date -format "MMddyyyyhmmss"
 $savename="blips_118379463"
 $localbackupsave="$savename_$date.bak"
 $savegameinfosave="SaveGameInfo_$date.bak"
--- ToStardew.ps1
+++ ToStardew.ps1
@@ -1,11 +1,13 @@
 $userprofile=$Env:USERPROFILE
 $savename="blips_118379463"
-$date=Get-Date -format "MM-dd-yyyy::h:mm-ss"
+$date=Get-Date -format "MMddyyyyhmmss"
+$localbackupsave="$savename_$date.bak"
 
 
 remove-item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename" -Recurse -Force
 start-sleep -s 1
 Copy-Item   "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" -Recurse "$userprofile\AppData\Roaming\StardewValley\Saves\" -Force
 rename-Item "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" "$savename_$date.bak" -Force
-move-item   "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename_$date.bak" "$userprofile\ownCloud\Freenas\Games\stardiew save\Backupsaves" 
+start-sleep -s 1
+move-item   "$userprofile\ownCloud\Freenas\Games\stardiew save\$localbackupsave" "$userprofile\ownCloud\Freenas\Games\stardiew save\Backupsaves\" 
 
