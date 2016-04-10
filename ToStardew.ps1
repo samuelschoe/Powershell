dcb1da2fcc7da0afa60dd9341642b851138c9829
anagramtogo <samuelschoe+bitbucket@gmail.com>
2016-04-10 11:43:05 -0700

    update date format and corrected backup location

--- ToStardew.ps1
+++ ToStardew.ps1
@@ -1,9 +1,11 @@
 $userprofile=$Env:USERPROFILE
 $savename="blips_118379463"
+$date=Get-Date -format "MM-dd-yyyy::h:mm-ss"
 
 
-remove-item $userprofile\AppData\Roaming\StardewValley\Saves\$savename\$savename
-remove-item $userprofile\AppData\Roaming\StardewValley\Saves\$savename\savebackup* -Force
-remove-item $userprofile\AppData\Roaming\StardewValley\Saves\$savename\SaveGameInfo
-start-sleep -s 3
-Copy-Item "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" -Recurse "$userprofile\AppData\Roaming\StardewValley\Saves\" -Force
+remove-item "$userprofile\AppData\Roaming\StardewValley\Saves\$savename" -Recurse -Force
+start-sleep -s 1
+Copy-Item   "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" -Recurse "$userprofile\AppData\Roaming\StardewValley\Saves\" -Force
+rename-Item "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename" "$savename_$date.bak" -Force
+move-item   "$userprofile\ownCloud\Freenas\Games\stardiew save\$savename_$date.bak" "$userprofile\ownCloud\Freenas\Games\stardiew save\Backupsaves" 
+
